c0d9e5651b7bc2a33f3a1f7a574a642e0179f9d5
Maenhout <victor@maenhout.fr>
2021-07-22 11:03:46 +0200

    Création du backend

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 
 const app = express();
+const { db } = require('./conf');
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
+app.get('/chapters/:id', async (req, res) => {
+  const { id } = req.params;
+  const sql = 'SELECT id, text FROM chapters WHERE id=?';
+  const sqlValues = [id];
+  const [chapters] = await db.query(sql, sqlValues);
+
+  if (chapters.length) {
+    res.json(chapters);
+  } else {
+    res.status(404).send('404 not found');
+  }
 });
 
-app.use('/', (req, res) => {
-  res.status(404).send('Route not found! ');
+app.get('/choices', async (req, res) => {
+  const { idChapter } = req.query;
+  const sql =
+    'SELECT id, text, idChapter, idTarget FROM choices  WHERE idChapter=?';
+  const sqlValues = [idChapter];
+  const [choices] = await db.query(sql, sqlValues);
+  res.json(choices);
 });
 
 app.listen(5050, () => {
-  console.log('Terra Battle API now available on http://localhost:5050 !');
+  console.log('Ravsgalat API now available on http://localhost:5050 !');
 });
