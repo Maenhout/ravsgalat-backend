4c936dad2ba7da3465f6a5de1b5cc3209ca33c57
Maenhout <victor@maenhout.fr>
2021-08-23 13:19:37 +0200

    Choices défini par id du Chapter associer

--- index.js
+++ index.js
@@ -16,13 +16,18 @@ app.get('/chapters/:id', async (req, res) => {
   }
 });
 
-app.get('/choices', async (req, res) => {
-  const { idChapter } = req.query;
+app.get('/choices/:idChapter', async (req, res) => {
+  const { idChapter } = req.params;
   const sql =
     'SELECT id, text, idChapter, idTarget FROM choices  WHERE idChapter=?';
   const sqlValues = [idChapter];
   const [choices] = await db.query(sql, sqlValues);
-  res.json(choices);
+
+  if (choices.length) {
+    res.json(choices);
+  } else {
+    res.status(404).send('404 not found');
+  }
 });
 
 app.listen(5050, () => {
